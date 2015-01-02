adf320a77c435991883c0ce3ccc98e37098e800e
AriaMoKr <kraft.aria@gmail.com>
2015-01-01 21:17:35 -0800

    misc refactorings

--- canvas/main.erl
+++ canvas/main.erl
@@ -4,13 +4,17 @@
 blocksize() -> 50.
 gridcount() -> 10.
 
-rects() -> io_lib:format("{\"x\":~p,\"y\":~p,\"w\":~p,\"h\":~p}",
+rects() -> 
+  random:seed(now()),
+  io_lib:format("{\"x\":~p,\"y\":~p,\"w\":~p,\"h\":~p}",
   [blocksize()*(random:uniform(gridcount())-1),
    blocksize()*(random:uniform(gridcount())-1),
    blocksize(), blocksize()]).
 
-initf() -> %"{\"blocksize\":20,\"gridcount\":20}".
+init() -> 
   io_lib:format("{\"blocksize\":~p,\"gridcount\":~p}",
                 [blocksize(), gridcount()]).
 
-start() -> yaws:start_embedded(".", [{listen, {0,0,0,0}}, {port, 8080}], [{cache_refresh_secs, 0}]).
+start() ->
+  yaws:start_embedded(".", [{listen, {0,0,0,0}}, {port, 8080}],
+                      [{cache_refresh_secs, 0}]).
